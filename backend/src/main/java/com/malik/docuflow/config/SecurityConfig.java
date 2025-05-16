e729f13b09204b9ec0c7bc02bf443ed049ddc4db
Malik Ibo <malik97un@gmail.com>
2025-05-16 02:55:43 +0200

    Mise à jour de SecurityConfig pour Render

--- backend/src/main/java/com/malik/docuflow/config/SecurityConfig.java
+++ backend/src/main/java/com/malik/docuflow/config/SecurityConfig.java
@@ -16,11 +16,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+            .cors(Customizer.withDefaults())
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/**").permitAll()
                 .anyRequest().permitAll()
             )
-            .httpBasic(httpBasic -> httpBasic.disable()); // optionnel, désactive l'authentification par formulaire
+            .httpBasic(http -> http.disable())
+            .formLogin(form -> form.disable());
 
         return http.build();
     }
@@ -28,8 +31,10 @@ public class SecurityConfig {
     @Bean
     // plus d'infos sur CORS: https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/cors/CorsConfiguration.html
     public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // ajoute l'URL pour appeler l'API
+        config.setAllowedOrigins(Arrays.asList(
+            "http://localhost:3000",
+            "https://docuflow.onrender.com"
+        )); 
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE")); // ajoute les méthodes HTTP que l'on veux autoriser
         config.setAllowedHeaders(Arrays.asList("*")); // * pour autoriser tous les headers. Un header c'est une information envoyée par le client au serveur (ex: Authorization, Content-Type, etc.)
         config.setAllowCredentials(true); // si tu veux autoriser les cookies/session
